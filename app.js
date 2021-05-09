e62d80f70c79923c2d8b03eb9961c582795c8847
Amy Shao <amys@ensemble.com>
2021-05-09 04:30:35 -0700

    redirect server to index.js

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ router.get("/getScores/:roomId", function (req, res) {
 });
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/src/index.html'), function(err) {
+    res.sendFile(path.join(__dirname, 'client/src/index.js'), function(err) {
       if (err) {
         res.status(500).send(err)
       }
